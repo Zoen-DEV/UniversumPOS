f809e0b51b050803c4d94bc249ca21fe8ecfc165
Zoen-DEV <enzoholgadodev@gmail.com>
2025-02-21 16:30:55 -0500

    first components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import MainViewCards from "./components/auth/cards";
 
-export default function App() {
+export default function HomeScreen() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+      colors={["#252057", "#0C0A26"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
+    >
+      {/* Header */}
+      <View style={styles.header}>
+        <View style={styles.headerInfo}>
+          <Text style={styles.title}>Bistro Restaurant</Text>
+          <Text style={styles.subtitle}>Select an option to start</Text>
+        </View>
+
+        <Text style={styles.headerBranch}>Universum POS</Text>
+      </View>
+
+      {/* Tarjetas */}
+      <MainViewCards />
+
+      {/* Footer */}
+      <Text style={styles.footer}>© Universum Restaurant</Text>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+    paddingTop: StatusBar.currentHeight,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+    width: "100%",
+    borderColor: "red",
+    flexWrap: "wrap-reverse",
+    gap: 10,
+  },
+  headerInfo: {
+    flexDirection: "column",
+    gap: 18,
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: 700,
+    color: "#fff",
+  },
+  subtitle: {
+    fontSize: 17,
+    color: "#808080",
+  },
+  headerBranch: {
+    fontSize: 20,
+    fontWeight: 500,
+    lineHeight: 25,
+    color: "#E8E8E8",
+    textAlign: "right",
+  },
+  footer: {
+    color: "#bbb",
+    width: "100%",
   },
 });
---
+++ components/auth/cards.tsx
@@ -0,0 +1,116 @@
+import {
+  Image,
+  Text,
+  TouchableOpacity,
+  useWindowDimensions,
+  View,
+  StyleSheet,
+} from "react-native";
+
+export default function MainViewCards() {
+  const { width } = useWindowDimensions();
+
+  return (
+    <View
+      style={width > 768 ? styles.cardContainer : styles.cardContainerSmall}
+    >
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("navigate to clock-in or clock-out time")}
+      >
+        <Image
+          source={require("../../assets/images/skater.png")}
+          style={width > 768 ? styles.cardLongImage : styles.cardSmallImage}
+        />
+
+        <View style={styles.cardInfo}>
+          <Text style={width > 768 ? styles.cardTitle : styles.cardTitleSmall}>
+            Entrada / salida
+          </Text>
+          <Text style={styles.cardText}>
+            Tap to set your clock-in or clock-out time
+          </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.card}
+        onPress={() => console.log("navigate to login")}
+      >
+        <Image
+          source={require("../../assets/images/login.png")}
+          style={width > 768 ? styles.cardLongImage : styles.cardSmallImage}
+        />
+
+        <View style={styles.cardInfo}>
+          <Text style={width > 768 ? styles.cardTitle : styles.cardTitleSmall}>
+            Log in
+          </Text>
+          <Text style={styles.cardText}>Tap to log in to your dashboard</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flexDirection: "row",
+    gap: 20,
+    height: "75%",
+    width: "100%",
+    justifyContent: "center",
+  },
+  cardContainerSmall: {
+    flexDirection: "row",
+    gap: 20,
+    height: "50%",
+    width: "100%",
+    justifyContent: "center",
+  },
+  card: {
+    backgroundColor: "rgba(255, 255, 255, 0.07)",
+    borderRadius: 18,
+    padding: 10,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "25%",
+    minWidth: 170,
+    gap: 20,
+    overflow: "hidden",
+  },
+  cardLongImage: {
+    width: "100%",
+    height: "60%",
+    marginBottom: 10,
+  },
+  cardSmallImage: {
+    width: "100%",
+    height: "40%",
+    marginBottom: 10,
+  },
+  cardInfo: {
+    flexDirection: "column",
+    alignItems: "center",
+    height: "40%",
+    width: "100%",
+    gap: 10,
+  },
+  cardTitle: {
+    fontSize: 32,
+    fontWeight: 700,
+    color: "#fff",
+  },
+  cardTitleSmall: {
+    fontSize: 18,
+    fontWeight: 700,
+    color: "#fff",
+  },
+  cardText: {
+    fontSize: 17,
+    color: "#808080",
+    textAlign: "center",
+    width: "70%",
+  },
+});
